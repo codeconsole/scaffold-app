a82315645f31cc6604c3d6c4479a9b5bf5ca214d
Scott Murphy Heiberg <scott@alwaysvip.com>
2025-10-06 13:49:41 -0700

    Demonstrate grails.importJavaTime is not working

--- grails-app/domain/com/example/User.groovy
+++ grails-app/domain/com/example/User.groovy
@@ -5,8 +5,6 @@ import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
-import java.time.LocalDateTime
-
 class User implements UserDetails {
 
     String firstName
